c5d2b5c168d40b9bef13e467e1ad8c07a4f2ecbf
Gaurav Ghongde <7gauravghongde@gmail.com>
2018-03-25 08:36:13 +0530

    change in launcher activity

--- app/src/main/java/com/rstack/dephone/MainActivity.java
+++ app/src/main/java/com/rstack/dephone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rstack.dephone;
 
+import android.app.usage.UsageStatsManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,23 +11,12 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
-    FirebaseAuth.AuthStateListener mAuthStateListener;
-
     private Button mButLogin;
     private Button mButSetting;
-
-    boolean flag;
 
     Intent mServiceIntent;
     private SensorService mSensorService;
@@ -48,54 +38,24 @@ public class MainActivity extends AppCompatActivity {
         mButLogin = findViewById(R.id.btn_signin);
         mButSetting = findViewById(R.id.settings);
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+        UsageStatsManager mUsageStatsManager = (UsageStatsManager)getSystemService(this.USAGE_STATS_SERVICE);
+
+
+        //startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+
+        mButSetting.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user!=null){
-                    Log.d("auth",user.getUid());
-
-                    //startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
-
-                    mButSetting.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent settingsPage = new Intent(MainActivity.this,AppSettingsActivity.class);
-                            startActivity(settingsPage);
-                        }
-                    });
-
-                    mSensorService = new SensorService(getCtx());
-                    mServiceIntent = new Intent(ctx, mSensorService.getClass());
-                    if (!isMyServiceRunning(mSensorService.getClass())) {   //starts if isn't already running
-                        startService(mServiceIntent);
-                    }
-
-
-                }
-                else{
-                    Intent welcomeScreen = new Intent(MainActivity.this, WelcomeActivity.class);
-                    //welcomeScreen.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(welcomeScreen);
-                }
+            public void onClick(View v) {
+                Intent settingsPage = new Intent(MainActivity.this,AppSettingsActivity.class);
+                startActivity(settingsPage);
             }
-        };
+        });
 
-        Log.i("flag123",Boolean.toString(flag));
-
-        //TODO:Welcome screen
-        if(!flag){
-            Intent welcomeScreen = new Intent(this, WelcomeActivity.class);
-            //welcomeScreen.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(welcomeScreen);
-            flag=getIntent().getExtras().getBoolean("flag");
-            Log.i("flag123","resume not flag"+Boolean.toString(flag));
-        }
-
-        if(flag){
-            butSignIn();
-            Log.i("flag123",Boolean.toString(flag)+" in else");
+        mSensorService = new SensorService(getCtx());
+        mServiceIntent = new Intent(ctx, mSensorService.getClass());
+        if (!isMyServiceRunning(mSensorService.getClass())) {   //starts if isn't already running
+            startService(mServiceIntent);
         }
 
     }
@@ -113,28 +73,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthStateListener);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mAuth.removeAuthStateListener(mAuthStateListener);
     }
 
-    public void butSignIn(){
-        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(!task.isSuccessful()) {
-                    Log.w("Auth", task.getException());
-                    Toast.makeText(MainActivity.this,"SignInProblem",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+
 
     @Override
     protected void onDestroy() {
--- app/src/main/java/com/rstack/dephone/WelcomeActivity.java
+++ app/src/main/java/com/rstack/dephone/WelcomeActivity.java
@@ -1,19 +1,36 @@
 package com.rstack.dephone;
 
+import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import com.rstack.dephone.MainActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private Button mGetStarted;
     private CheckBox mTnCchkBox;
     boolean flag = false;
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,25 @@ public class WelcomeActivity extends AppCompatActivity {
         mGetStarted = findViewById(R.id.get_sarted_btn);
         mTnCchkBox = findViewById(R.id.tandc_chkbox);
         mGetStarted.setEnabled(false);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Log.d("auth",user.getUid());
+                    Intent mainActivityIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    mainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(mainActivityIntent);
+                    finish();
+                }
+            }
+        };
+
+        if(!checkForPermission(WelcomeActivity.this)){
+            startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+        }
 
         mTnCchkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -39,15 +75,45 @@ public class WelcomeActivity extends AppCompatActivity {
         mGetStarted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                flag = true;
-                Intent i = new Intent();
-                i.putExtra("flag",flag);
-                setResult(RESULT_OK,i);
-                finish();
+                butSignIn();
             }
         });
 
+    }
+
+
+    private boolean checkForPermission(Context context) {
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), context.getPackageName());
+        return mode == MODE_ALLOWED;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    public void butSignIn(){
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()) {
+                    Log.w("Auth", task.getException());
+                    Toast.makeText(WelcomeActivity.this,"SignInProblem",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent mainActivityIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(mainActivityIntent);
+                    finish();
+                }
+            }
+        });
     }
 }
